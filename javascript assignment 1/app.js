f4dd280a52bc19493bcfe65143f7b6f9a4a65247
ABDULRAAFAAYY <130109622+ABDULRAAFAAYY@users.noreply.github.com>
2023-05-19 19:06:07 +0500

    Add files via upload

---
+++ javascript assignment 1/app.js
@@ -0,0 +1,237 @@
+// // CHAP 1 QUESTIONS 
+// // Q1; 
+// // alert("First name");
+
+// // alert("Last name ");
+
+// // alert("Phone Number");
+
+// // alert("Email Address");
+
+
+
+// // alert("PASSWORD");
+
+// // // Q2;
+// // alert("You're learning Javascript");
+
+// // // Q3
+// // alert("Pakistan has won the WORLDCUP23 in India")
+
+// // CHAP 2 QUESTIONS
+// // Q1
+// var aB = "kaRAChi";
+// console.log(aB);
+
+// // Q2
+// var mychoice;
+// mychoice="icecream";
+// console.log(mychoice);
+
+// // Q3
+// var TeamName = "FC Barcelona";
+// alert("Team Name is FC Barcelona ");
+// console.log(TeamName);
+
+// // Q4
+// var bestMan = "Charlie";
+// bestMan=="Chaplin";
+// console.log(bestMan);
+
+// // CHAP4 QUESTIONS
+// // Q1
+// var caseQty = 123;
+
+// // Q2
+// var caseQty = 144
+// console.log(caseQty);
+
+// // Q3
+// var num = 9;
+
+//     // Q4
+//     var add;
+//     add=45+44
+//     console.log(add);
+
+//     // Q5
+//     var merchtotal=110;
+//     var shippingcharge=20
+//     totalcharge=merchtotal+shippingcharge;
+//     console.log(totalcharge);
+
+//     // Q6
+//     var bc= 33
+//     cc=bc+32;
+//     console.log(cc);
+
+//     // CHAP4 QUESTION
+//     // Q1
+//     var productcost = 3.45;
+
+//     // Q2
+//     // REWRITE
+//     var nameOFband;
+
+//     // Q3
+//      var myArr = 72;
+
+//     //  Q4
+//     var aBdUlRAFAy;
+
+//     // Q5
+//     // no javascript keywords no wordspaces
+//     // all things like letters, numbers, dollar sign etc  
+
+//     // CHAP5 QUESTIONS
+//     // Q1
+//     // % IS THE SYMBOL THAT GIVES THE REMINDER THAT ONE NUMBER IS DIVIDED BY ANOTHER
+
+//     // Q2
+//     var numb=20%6;
+//     console.log(numb);
+
+//     // Q3
+//     var largeNum =1000*2000;
+//     console.log(largeNum);
+
+//     // Q4
+//     var n = 66;
+//     var m = 22;
+//     o = n-m;
+// console.log(o);
+
+// // Q5
+// var multi = 11*11;
+// alert("THE ANSWER IS 121 ");
+
+// // CHAP6 QUESTIONS
+// // Q1
+
+// var x = 92;
+// x=92+1;
+// console.log(x);
+
+// // Q2
+// var X = 100;
+// var Y =1;
+// Z = X - Y;
+// console.log(Z);
+
+// // Q3&4
+// var x = 50;
+// var y = x++;
+// console.log(y);
+
+// var z=--y;
+// console.log(z);
+ 
+// // Q5&6
+
+// var numB= 1;
+// var newnumb=++numB;
+
+// var nUmB= 1;
+// var neWnumb=--nUmB;
+
+// // Q7
+
+// var newno=52;
+// g = ++newno;
+
+// // CHAP7
+// // Q1
+// var calculatednum = 2 + (2*6);
+// console.log(calculatednum);
+
+// // Q2
+// var calculatednUm = (2+2)*6;
+// console.log(calculatednUm);
+ 
+// // Q3
+// var calculatedNUm = (2+2)*(4+2);
+// console.log(calculatedNUm);
+
+// // Q4
+// var cAlculatedNUm = ((2+2)*4)+2;
+// console.log(cAlculatedNUm);
+
+// // Q5
+// var staTE = 2 +2*4+10;
+// console.log(staTE);
+
+// var statE = 4/2 *4;
+// console.log(statE);
+
+// // CHAP 8 
+// // Q1
+// var value = "2"+ "2"
+// console.log (value);
+
+// // Q2
+//  var message = ("hello" + "dolly");
+// alert(message);
+
+// // Q3
+// alert("33" + 3);
+
+// // Q4
+// var dd = "Pakistan";
+// var ee = "zindabad";
+// var ff = dd + ee ;
+// alert(ff);
+
+// // Q5&6
+// var k = "a";
+// var j = "2";
+// var l = k + j;
+// console.log(l);
+
+// // CHAP9
+// // Q1
+// var  user = prompt("Enter your name");
+
+// // Q2
+// var question = "COUNTRY?";
+// var defaultanswer = "CHINA";
+// var answer = prompt("question","defaultanswer");
+// // Q3
+// var yourNAMe=prompt("ENTER YOUR NAME");
+
+// // Q4
+// // var question = "COUNTRY?";
+// // var defaultanswer = "CHINA";
+// // var answer = prompt("question","defaultanswer");
+ 
+// // Q5
+// var question = "Your species?";
+//  var defaultAnswer = "human";
+//  var spec = prompt(question, defaultAnswer);
+
+
+// //  chap9 Q1
+//  var city = "Karachi"
+// if (city = "Karachi") {
+//     console.log("The City OF Lights") };
+//  Q2
+//  var x = 33;
+//  var y = 33
+//  if(x===y){
+//     prompt("THE VALUE OF Z");
+//  }
+// //  Q3
+
+// var zipcode = 10011;
+// if(zipcode==10010){
+//     alert("KARACHI")
+// };
+// if(zipcode==10011){
+//     alert("PLEASE ENTER CORRECT CITY")
+// };
+
+// Q4
+var x = 1;
+if(x===1){
+    x=2;
+}
+console.log(x);
